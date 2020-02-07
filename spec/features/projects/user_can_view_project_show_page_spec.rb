9b0ff480369ef170c0bec6689925ee3f203983af
William Meighan <williamdmeighan@gmail.com>
2020-02-07 10:40:09 -0700

    Add test for total number of contestants and update before each

--- spec/features/projects/user_can_view_project_show_page_spec.rb
+++ spec/features/projects/user_can_view_project_show_page_spec.rb
@@ -5,7 +5,11 @@ describe 'As a visitor' do
 
     before :each do
       @challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
       @project = @challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+
+      @contestant_1 =  @project.contestants.create(name: 'Will', age: 27, hometown: 'Ankeny', years_of_experience: 1)
+      @contestant_2 =  @project.contestants.create(name: 'Bobby', age: 13, hometown: 'Denver', years_of_experience: 13)
 
       visit "/projects/#{@project.id}"
     end
@@ -18,5 +22,10 @@ describe 'As a visitor' do
     it 'I can see the theme of the challenge that the project belongs to' do
       expect(page).to have_content("Apartment Furnishings")
     end
+
+    it 'I can see the number of contestants on this project' do
+      expect(page).to have_content('Total number of contestants; 2')
+    end
+
   end
 end
