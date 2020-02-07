ba0ff5d152d6c66fb2b1441e7da83652e298b185
William Meighan <williamdmeighan@gmail.com>
2020-02-07 11:00:04 -0700

    add model test for average years of experience

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -21,4 +21,13 @@ RSpec.describe Project, type: :model do
 
     expect(project.contestant_count).to eq(2)
   end
+
+  it 'should calculate average years of experience' do
+    challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+    project = challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+    contestant_1 = project.contestants.create(name: 'Will', age: 27, hometown: 'Ankeny', years_of_experience: 1)
+    contestant_2 = project.contestants.create(name: 'Bobby', age: 13, hometown: 'Denver', years_of_experience: 13)
+
+    expect(project.average_years_of_experience).to eq(7)
+  end
 end
