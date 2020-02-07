22f021e0a21d038796f5bcda8176027cc8954a36
William Meighan <williamdmeighan@gmail.com>
2020-02-07 09:50:47 -0700

    Fix test to use before and use project id

--- spec/features/user_can_view_project_show_page_spec.rb
+++ spec/features/user_can_view_project_show_page_spec.rb
@@ -2,15 +2,21 @@ require 'rails_helper'
 
 describe 'As a visitor' do
   describe 'when I visit the project show page' do
+
+    before :each do
+      @challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+      @project = @challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+
+      visit "/projects/#{@project.id}"
+    end
+
     it 'I can see the projects name and material' do
-
-      furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
-      furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
-
-      visit "/projects/#{furniture_challenge.id}"
-
       expect(page).to have_content("Upholstery Tuxedo")
       expect(page).to have_content("Couch")
+    end
+
+    it 'I can see the theme of the challenge that the project belongs to' do
+      expect(page).to have_content("Apartment Furnishings")
     end
   end
 end
