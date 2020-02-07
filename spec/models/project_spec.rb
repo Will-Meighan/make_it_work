c2dfa6b57b1672d55f662e91c07b76bb6f3dfabc
William Meighan <williamdmeighan@gmail.com>
2020-02-07 10:44:05 -0700

    create model test for count of contestants

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,4 +12,13 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  it 'should count total number of contestants on a project' do
+    challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+    project = challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+    contestant_1 = project.contestants.create(name: 'Will', age: 27, hometown: 'Ankeny', years_of_experience: 1)
+    contestant_2 = project.contestants.create(name: 'Bobby', age: 13, hometown: 'Denver', years_of_experience: 13)
+
+    expect(project.contestant_count).to eq(2)
+  end
 end
