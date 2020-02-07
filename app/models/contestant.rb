fa853c6965f67f99b93b6b26aef8a5ab2e16e143
William Meighan <williamdmeighan@gmail.com>
2020-02-07 09:35:04 -0700

    All model tests passing

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -1,3 +1,9 @@
 class Contestant <ApplicationRecord
+  validates_presence_of :name
+  validates_presence_of :age
+  validates_presence_of :hometown
+  validates_presence_of :years_of_experience
 
+  has_many :contestant_projects
+  has_many :projects, through: :contestant_projects
 end
